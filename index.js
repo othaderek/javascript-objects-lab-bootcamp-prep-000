0fe344166cec898acbc213b364a4cf9fd4ab79d8
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-06 03:50:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
 
 function deleteFromObjectByKey(recipes, key) {
   var newObj = Object.assign ({}, recipes)
-  delete newObj.key
+  delete newObj[key]
   return newObj
 }
 
