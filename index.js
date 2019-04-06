a816f266e08be1c2e99f8839edb181fe9821bdd8
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-06 03:07:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ function updateObjectWithKeyAndValue(recipes, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
-  
+  recipes[key] = value
+  return recipes
 }
 
 function deleteFromObjectByKey(recipes, key) {
